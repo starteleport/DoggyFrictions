0f5aefaf3457123e049d0caf5e4f3f647a7ad196
starteleport <starteleport@yandex.ru>
2016-06-11 02:29:10 +0500

    Domain draft

---
+++ DoggyFriction/Domain/Action.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoggyFriction.Domain
+{
+    public class Action
+    {
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<Participation> Sponsors { get; set; }
+        public IEnumerable<Consumption> Consumptions { get; set; }
+    }
+}
---
+++ DoggyFriction/Domain/SessionActionsProvider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DoggyFriction.Models;
+
+namespace DoggyFriction.Domain
+{
+    public class SessionActionsProvider
+    {
+        public IEnumerable<Action> GetSessionActions(SessionModel session, IEnumerable<ActionModel> actions)
+        {
+            var participants = session.Participants.ToDictionary(p => p.Id, p => p.Name);
+            return actions.Select(
+                a =>
+                    new Action
+                    {
+                        Date = a.Date,
+                        Description = a.Description,
+                        Consumptions = a.Consumptions.Select(c => MapConsumption(c, participants)),
+                        Sponsors = a.Payers.Select(p => MapPayer(p, participants))
+                    });
+        }
+
+        private Participation MapConsumer(ConsumerModel consumer, Dictionary<int, string> participants)
+            => new Participation {Amount = consumer.Amount, Participant = participants[consumer.ParticipantId]};
+
+        private Participation MapPayer(PayerModel payer, Dictionary<int, string> participants)
+            => new Participation {Amount = payer.Amount, Participant = participants[payer.ParticipantId]};
+
+        private Consumption MapConsumption(ConsumptionModel consumption, Dictionary<int, string> participants)
+        {
+            return new Consumption
+            {
+                Description = consumption.Description,
+                PricePerUnit = consumption.Amount,
+                Quantity = consumption.Quantity,
+                SplittedEqually = consumption.SplittedEqually,
+                Consumers = consumption.Consumers.Select(c => MapConsumer(c, participants))
+            };
+        }
+    }
+}
